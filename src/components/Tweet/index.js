b18216da37fff8ba39ce35f0280b35cdcffede38
Zeca Cordeiro <zecaxcr@gmail.com>
2022-09-02 19:52:33 -0300

    add components of tweets

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Retweeted,
+  UserIcon,
+  Body,
+  Avatar,
+  Content,
+  Header,
+  Dot,
+  Description,
+  ImageContent,
+  Icons,
+  Status,
+  CommentIcon,
+  RetweetIcon,
+  LiketIcon,
+} from './styles';
 
 const Tweet = () => {
-    return ( <Container>
+  return (
+    <Container>
+      <Retweeted>Você retweetou</Retweeted>
+      <UserIcon />
+      <Body>
+        <Avatar />
+        <Content>
+          <Header>
+            <strong>Usuário</strong>
+            <span>@usuario123</span>
+            <Dot />
+            <time>02 de setembro</time>
+          </Header>
 
-    </Container> );
-}
- 
+          <Description>Descrição simples de teste</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              27
+            </Status>
+            <Status>
+              <RetweetIcon />
+              89
+            </Status>
+            <Status>
+              <LiketIcon />
+              376
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+};
+
 export default Tweet;
--- src/components/Tweet/styles.js
+++ src/components/Tweet/styles.js
@@ -1,3 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div``
+export const Container = styled.div``;
+export const Retweeted = styled.div``;
+export const UserIcon = styled.div``;
+export const Body = styled.div``;
+export const Avatar = styled.div``;
+export const Content = styled.div``;
+export const Header = styled.div``;
+export const Dot = styled.div``;
+export const Description = styled.div``;
+export const ImageContent = styled.div``;
+export const Icons = styled.div``;
+export const Status = styled.div``;
+export const CommentIcon = styled.div``;
+export const RetweetIcon = styled.div``;
+export const LiketIcon = styled.div``;
